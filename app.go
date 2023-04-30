351de72190d45b2805d680fd4347291baf443f30
adityarizkyramadhan <adityarizky1020@gmail.com>
2023-04-30 17:24:35 +0700

    ✨ feat(school,student): add constructor

--- app.go
+++ app.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	rpSchool "u-future-api/api/school/repository"
+	ucSchool "u-future-api/api/school/usecase"
+	rpStudent "u-future-api/api/student/repository"
+	ucStudent "u-future-api/api/student/usecase"
 	"u-future-api/database/mysql"
 	"u-future-api/middleware"
+	"u-future-api/models"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -22,9 +27,27 @@ func main() {
 	if db == nil {
 		log.Fatal("init connection db failed")
 	}
-	err = mysql.Migrate()
+	err = mysql.Migrate(new(models.School), new(models.Student))
 	if err != nil {
 		log.Fatal(err.Error())
+	}
+
+	repoSchool := rpSchool.New(db)
+	useCaseSchool := ucSchool.New(repoSchool)
+
+	repoStudent := rpStudent.New(db)
+	useCaseStudent := ucStudent.New(repoStudent)
+
+	// Generate Faker School
+	err = useCaseSchool.GenerateFaker()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	// Generate Faker Student
+	err = useCaseStudent.GenerateFaker(useCaseSchool)
+	if err != nil {
+		log.Fatalln(err.Error())
 	}
 
 	gin.SetMode(gin.DebugMode)
